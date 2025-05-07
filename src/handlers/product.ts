529cb9d015f918df254b8e1cfcd940b51d3f71dc
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-06 22:52:26 -0300

    feat: implement product retrieval and update functionality with error handling

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,10 +1,62 @@
 import { Request, Response } from "express"
 import Product from "../models/Product.model"
 
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+
+        const products = await Product.findAll({
+            order: [
+                ["price", "ASC"]
+            ],
+            attributes: { exclude: ["createdAt", "updatedAt", "availability"] },
+        })
+        res.json({ data: products })
+    } catch (error) {
+
+        console.log(error)
+
+    }
+}
+
+export const getProductById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        console.log("desde getProductById")
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if (!product) {
+            res.status(404).json({ message: "Producto no encontrado" })
+            return
+        }
+
+    } catch (error) {
+
+        console.log(error)
+
+    }
+}
 
 export const createProduct = async (req: Request, res: Response) => {
     try {
         const product = await Product.create(req.body)
+        res.json({ data: product })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if (!product) {
+            res.status(404).json({ message: "Producto no encontrado" })
+            return
+        }
+
+        await product.update(req.body)
+        await product.save()
         res.json({ data: product })
     } catch (error) {
         console.log(error)
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { body } from "express-validator";
-import { createProduct } from "./handlers/product";
+import { body, param } from "express-validator";
+import { createProduct, getProductById, getProducts, updateProduct } from "./handlers/product";
 import { handleInputErrors } from "./middleware";
 const router = Router()
 
 //Routing
-router.get("/", (req, res) => {
-    res.json("desde GET")
-})
+router.get("/", getProducts)
+router.get("/:id",
+    param("id").isInt().withMessage("El id debe ser un numero entero"),
+    handleInputErrors,
+    getProductById
+)
 
 router.post("/",
     //validacion de datos   
@@ -22,9 +25,20 @@ router.post("/",
     createProduct
 )
 
-router.put("/", (req, res) => {
-    res.json("desde PUT")
-})
+router.put("/:id",
+    //validacion de datos   
+    body("name")
+        .notEmpty().withMessage("El nombre del producto no puede estar vacio"),
+
+    body("price")
+        .isNumeric().withMessage("valor no valido")
+        .notEmpty().withMessage("El precio del producto no puede estar vacio")
+        .custom(value => value > 0).withMessage("precio no valido"),
+    body("availability")
+        .isBoolean().withMessage("El valor de disponibilidad debe ser true o false"),
+    handleInputErrors,
+    updateProduct,
+)
 
 router.patch("/", (req, res) => {
     res.json("desde PATCH")

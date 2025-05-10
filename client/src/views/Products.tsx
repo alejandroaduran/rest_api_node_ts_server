8d7386075b2bf50e8c2bbce82913ac12e39db644
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-09 22:42:49 -0300

    feat: implement product registration form and update layout with header

--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom"
+
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500"> Productos</h2>
+        <Link
+        to={"/productos/nuevo"}
+        className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white hover:bg-indigo-700 transition-colors"
+        >
+          Agregar Productos
+        </Link>
+      </div>
+    </>
   )
 }

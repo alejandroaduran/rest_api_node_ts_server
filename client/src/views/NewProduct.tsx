723ca80a226a22ebd8195e1bb294acc41d22b214
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-09 22:52:40 -0300

    feat: add form validation to NewProduct component

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,11 +1,24 @@
-import { Link, Form } from "react-router-dom";
+import { Link, Form, useActionData } from "react-router-dom";
 
-export async function action() {
-    console.log("action");
+export async function action({ request }) {
+    const data = Object.fromEntries(await request.formData());
+    console.log(data);
+    let error = ""
+    if (Object.values(data).includes("")) {
+        error = "Todos los campos son obligatorios"
+    }
+if(error.length){
+    return error
+}
+
     return null;
 }
 
 export default function NewProduct() {
+
+const error = useActionData();
+console.log(error);
+
     return (
         <>
             <div className="flex justify-between">

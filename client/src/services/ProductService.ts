1020399f5d14676feba68156144e15f117d0e179
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-11 23:38:10 -0300

    feat: add axios for API calls and implement addProduct function with validation

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,5 +1,27 @@
-type productData = {[k: string]: FormDataEntryValue}
+import axios from "axios";
+import { DraftProductSchema } from "../types";
+import { safeParse } from "valibot";
+type productData = { [k: string]: FormDataEntryValue }
 
-export async function addProduct(data: productData ) {
-    console.log(data);
+export async function addProduct(data: productData) {
+    //console.log(data);
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        });
+        console.log(result);
+        if (result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products`;
+            const { data } = await axios.post(url, {
+                name: result.output.name,
+                price: result.output.price
+            })
+        } else {
+            throw new Error("Invalid data");
+        }
+    } catch (error) {
+        console.error("Error adding product:", error);
+    }
+
 }
